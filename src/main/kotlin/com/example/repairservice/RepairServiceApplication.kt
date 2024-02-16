d4dff86dc99e2dd1ee1dd1c8c3541b0b01e20c31
IvanKhrustalev <khrustalev16@gmail.com>
2024-02-16 14:18:06 +0300

    init

---
+++ src/main/kotlin/com/example/repairservice/RepairServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.repairservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RepairServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<RepairServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/repairservice/RepairServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.repairservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RepairServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
